680b333f706aa827b826e6a8c4f74003221c3818
tetradog <tetradog@gmail.com>
2017-01-23 16:52:34 +0100

    subir archivos de momento va en el rojo y verde :D

---
+++ TrainerEditorUniversal/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gabriel.Cat.Extension;
+using PokemonGBAFrameWork;
+using Microsoft.Win32;
+
+namespace TrainerEditorUniversal
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RomData rom;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PideRom();
+            if (rom == null)
+                this.Close();
+        }
+
+        private void PideRom()
+        {
+            OpenFileDialog opn = new OpenFileDialog();
+            Image img;
+            opn.Filter = "GBA|*.gba";
+
+            if (opn.ShowDialog().GetValueOrDefault())
+            {
+
+                rom = new RomData(new RomGBA(opn.FileName));
+                Pokemon.Orden = Pokemon.OrdenPokemon.Nacional;
+                rom.Pokedex.Sort();
+                for (int i = 0; i < rom.Entrenadores.Count; i++)
+                {
+                    if (rom.Entrenadores[i].Pokemon.HayObjetosEquipados())
+                    {
+                        try
+                        {
+                            img = new Image();
+                            img.SetImage(rom.SpritesEntrenadores[rom.Entrenadores[i]]);
+                            img.Tag = rom.Entrenadores[i];
+                            img.MouseLeftButtonUp += PonEntrenador;
+                            ugEntrenadores.Children.Add(img);
+                        }
+
+                        catch { }
+                    }
+                }
+                Title = rom.RomGBA.NombreRom;
+
+            }
+        }
+
+        private void PonEntrenador(object sender, MouseButtonEventArgs e)
+        {
+            Entrenador entrenadorSeleccionado = ((Image)sender).Tag as Entrenador;
+            Title = entrenadorSeleccionado.Nombre;
+            ugEquipoEntrenador.Children.Clear();
+            for (int i = 0; i < entrenadorSeleccionado.Pokemon.PokemonEquipo.Length; i++)
+                if (entrenadorSeleccionado.Pokemon[i] != null)
+                    ugEquipoEntrenador.Children.Add(new PokemonEntrenador(rom, entrenadorSeleccionado.Pokemon[i]));
+        }
+    }
+}
---
+++ TrainerEditorUniversal/PokemonEntrenador.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PokemonGBAFrameWork;
+using Gabriel.Cat.Extension;
+namespace TrainerEditorUniversal
+{
+    /// <summary>
+    /// Lógica de interacción para PokemonEntrenador.xaml
+    /// </summary>
+    public partial class PokemonEntrenador : UserControl
+    {
+        public PokemonEntrenador(RomData rom,Entrenador.Equipo.Pokemon pokemon)
+        {
+            InitializeComponent();
+            try
+            {
+                    imgPokemon.SetImage(rom.Pokedex[pokemon.PokemonIndex].Sprites.GetImagenFrontal());
+                    txtNombre.Text = rom.Pokedex[pokemon.PokemonIndex].Nombre;
+                    imgObjeto.SetImage(rom.Objetos[pokemon.Item].ImagenObjeto);
+                
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+    }
+}
